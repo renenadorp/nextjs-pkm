b922290b5ea465f258c948dc7e7164089166aec9
Rene Nadorp <rene@rnadorp.nl>
2023-08-20 18:06:40 +0200

    standalone

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  
 })
 
+const standaloneNextConfig=
+  /**
+   * @type {import('next').NextConfig}
+   */
+  nextConfig = {
+    output: 'standalone',
+  };
+
 // module.exports = withNextra()
-module.exports = 'standalone'
+module.exports = withNextra({standaloneNextConfig})
+;
+
